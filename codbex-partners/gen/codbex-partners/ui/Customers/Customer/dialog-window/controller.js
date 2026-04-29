328eb0ef8ac7ba2dde1dbf32a662a89aec0e0822
NicoleNG18 <nicole.georgieva18@abv.bg>
2026-04-29 23:42:44 +0300

    add depends on in customer's city

--- codbex-partners/gen/codbex-partners/ui/Customers/Customer/dialog-window/controller.js
+++ codbex-partners/gen/codbex-partners/ui/Customers/Customer/dialog-window/controller.js
@@ -419,6 +419,35 @@ angular.module('page', ['blimpKit', 'platformView', 'platformLocale', 'EntitySer
 	}
 
 
+		$scope.$watch('entity.Country', (newValue, oldValue) => {
+			if (newValue !== undefined && newValue !== null) {
+				$http.get($scope.serviceCountry + '/' + newValue).then((response) => {
+					let valueFrom = response.data.Id;
+					$http.post('/services/ts/codbex-cities/gen/codbex-cities/api/Settings/CityController.ts/search', {
+						conditions: [
+							{ propertyName: 'Country', operator: 'EQ', value: valueFrom }
+						]
+					}).then((response) => {
+						$scope.optionsCity = response.data.map(e => ({
+							value: e.Id,
+							text: e.Name
+						}));
+						if ($scope.action !== 'select' && newValue !== oldValue) {
+							if ($scope.optionsCity.length == 1) {
+								$scope.entity.City = $scope.optionsCity[0].value;
+							} else {
+								$scope.entity.City = undefined;
+							}
+						}
+					}, (error) => {
+						console.error(error);
+					});
+				}, (error) => {
+					console.error(error);
+				});
+			}
+		});
+
 		$scope.alert = (message) => {
 			if (message) Dialogs.showAlert({
 				title: description,
